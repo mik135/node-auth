4f81528fb4c81c44f462d973c21d1d269efaf8f8
mik135 <michaelsavior1@gmail.com>
2024-02-09 11:57:14 +0100

    Add middleware and controller

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,10 +1,10 @@
 import jwt from "jsonwebtoken";
 import bcrypt from 'bcrypt'
-import User from "../models/User"
+import User from "../models/User.js"
 
 // Register a new user
-const register = async (req, res, next) => {
-    const { username, email, password} = req.body;
+export const register = async (req, res, next) => {
+    const { username, email, password } = req.body;
 
     try {
         const hashedPassword = await bcrypt.hash(password, 10);
@@ -16,10 +16,25 @@ const register = async (req, res, next) => {
     }
 };
 
-const login = async (req, res, next) => {
+export const login = async (req, res, next) => {
     const { username, password } = req.body;
 
     try {
         const user = await User.findOne({ username });
+        if(!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+
+        const passwordMatch = await user.comparePassword(password);
+        if(!passwordMatch) {
+            return res.status(401).json({ message: 'Incorrect password' })
+        }
+
+        const token = jwt.sign({ userId: user._id}, process.env.SECRET_KEY, {
+            expiresIn: '1 hour'
+        });
+        res.json({ token });
+    } catch (error) {
+        next(error)
     }
 }
--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 import express from "express"
 import db from "../db/conn.js"
+import authenticate from '../middlewares/auth.js'
 
 const userRoutes = express.Router()
 
@@ -8,6 +9,10 @@ userRoutes.get('/users', async (req, res) => {
     let collection = db.collection('users');
     let result = await collection.find({}).limit(10).toArray()
     res.send(result)
+})
+
+userRoutes.get('/profile', authenticate, (req, res) => {
+    res.json({ message: `Welcome ${req.user.username}` })
 })
 
 export default userRoutes;
